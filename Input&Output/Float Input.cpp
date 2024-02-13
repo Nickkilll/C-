fb8bf5d924596476ca5982fd82f3932ca9736647
Nickkilll <159870415+Nickkilll@users.noreply.github.com>
2024-02-13 16:09:43 +0530

    Create Float Input.cpp

---
+++ Input&Output/Float Input.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip> //for manipulation
+using namespace std;
+int main()
+{
+    float a;
+    cin>>a;
+    cout<<fixed<<setprecision(6)<<a;
+    return 0;
+    
+}
