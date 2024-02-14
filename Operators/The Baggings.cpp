38f03a0b6397c11451f0913395b970c810486dc1
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-14 21:06:53 +0530

    Create The Baggings.cpp

---
+++ Operators/The Baggings.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    int x,y,f,dis;
+    cin>>x>>y;
+    f=(x-3)*(x-3)+(y-4)*(y-4);
+    dis=sqrt(f);
+    cout<<dis;
+    return 0;
+
+}
