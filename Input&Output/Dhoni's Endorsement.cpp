534d7633fedf5e29a48d41b3594f6d56221a9afb
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-14 20:52:14 +0530

    Create Dhoni's Endorsement.cpp

---
+++ Input&Output/Dhoni's Endorsement.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+int main()
+{
+   std::cout<<"Pepsico"<<"\n"<<"AIRCEL"<<"\n"<<"Communications"<<"\n"<<"Titan"<<"\n"<<"TVS"<<"\n"<<"Motors"<<"\n"<<"Boost"<<"\n"<<"Reebok"<<"\n"<<"Videocon"<<"\n"<<"GE Money";
+    return 0;
+}
