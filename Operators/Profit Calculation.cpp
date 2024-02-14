4797bfe4f39f3f72a11edda420d027325d598e26
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-14 21:28:33 +0530

    Create Profit Calculation.cpp

---
+++ Operators/Profit Calculation.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,a,b,co;
+    cin>>x>>a>>b;
+    co=(x*a)-(x*b)-100;
+    cout<<"The profit obtained is Rs."<<co;
+    return 0;
+    
+    
+}
