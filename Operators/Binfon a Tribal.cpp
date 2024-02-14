0d262f587e950a5bb28f37e2e9a3f1d26088215b
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-14 21:06:28 +0530

    Create Binfon a Tribal.cpp

---
+++ Operators/Binfon a Tribal.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int days;
+    cin>>days;
+    cout<<(days/365)<<endl;
+    cout<<(days%365)/7<<endl;
+    cout<<(days%365)%7<<endl;
+    return 0;
+    
+    
+    
+}
