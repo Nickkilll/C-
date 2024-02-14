7cdc22ed3e8aeef079c95ba727493e4a00698cf1
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-14 20:51:35 +0530

    Create Dhoni's Family.cpp

---
+++ Input&Output/Dhoni's Family.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"M. S. Dhoni"<<"\n"<<"Father: Mr. Pan Singh"<<"\n"<<"Mother: Mrs. Devki Devi"<<"\n"<<"Sibings: Narendra Singh and Jayanti Gupta";
+    return 0;
+    
+}
