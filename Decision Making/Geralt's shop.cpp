7ecceba260111625461af26d6ffbc34e6ab4efd9
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-19 23:58:46 +0530

    Create Geralt's shop.cpp

---
+++ Decision Making/Geralt's shop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    string d;
+    cin>>d;
+    cin>>a>>b>>c;
+    cout<<"Amount for "<<c<<" "<<d<<" is "<<c*b<<"\nRemaining number of "<<d<<" present in stock is "<<a-c;
+    return 0;
+    
+}
