b34e40f470c415e0faaee11558eaafbf9cef5a13
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-19 23:57:13 +0530

    Create Savings and Expenses.cpp

---
+++ Decision Making/Savings and Expenses.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int a,b,c,d,e,ex;
+   cin>>a>>b>>c>>d>>e;
+   ex=b+c+d+e;
+   if((a-ex)<=0)
+   {
+       cout<<"He wants to work hard";
+       
+   }
+   else
+   {
+       cout<<"He can save the money";
+   }
+   return 0;
+
+}
