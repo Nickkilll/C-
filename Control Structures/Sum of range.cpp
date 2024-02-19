9933a08274e6435963d1cd0b5fc1de856dd5a1c3
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-20 00:04:14 +0530

    Create Sum of range.cpp

---
+++ Control Structures/Sum of range.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,op=0;
+    cin>>a>>b;
+    for(int i=a;i<=b;i++)
+    {
+        op=op+i;
+    }
+    cout<<op;
+    return 0;
+    
+}
