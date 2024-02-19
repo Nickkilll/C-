a162a7e481eeaea2b684e3a784281f38ae64d3b5
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-19 23:59:23 +0530

    Create BMI.cpp

---
+++ Decision Making/BMI.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float w,h,bmi,hi,hf;
+    cin>>w>>h;
+    hi=(h/100);
+    hf=hi*hi;
+    bmi= w/hf;
+    if(bmi<=18.5)
+    {
+        cout<<"You are underweight. Have an apple daily";
+        
+    }
+    else if(bmi>=19 && bmi<=24.9)
+    {
+        cout<<"You are normal. Go walking daily and maintain it.";
+    }
+    else
+    {
+        cout<<"You are obese. Go to doctor";
+        
+    }
+    return 0;
+}
