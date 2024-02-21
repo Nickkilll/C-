e49099582100d598458c4959751d5c8e6e0e766d
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-21 15:41:32 +0530

    Update Sum of Even and ODD.cpp

--- Control Structures/Sum of Even and ODD.cpp
+++ Control Structures/Sum of Even and ODD.cpp
@@ -19,3 +19,36 @@
 
 // 2.00
 
+
+#include<iostream>
+#include<iomanip>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a,b=0,avge;
+    float c=0,d=0,e=0,avgo=0;
+    {
+        while(a!=-1 )
+        {
+            cin>>a;
+            if(a%2==0 && a!=0)
+            {
+               b=b+a;
+               d++;
+            }
+              
+             if(a%2!=0 && a!=0)
+            {
+               c=c+a;
+               e++;
+            }
+        }
+        avgo=((c+1)/(e-1));
+        avge=(b/d);
+        cout<<b<<'\n';
+        cout<<c+1<<'\n';
+        cout<<fixed<<setprecision(2)<<avge<<'\n';
+        cout<<fixed<<setprecision(2)<<avgo<<'\n';
+    }
+}
