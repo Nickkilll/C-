aa7d52ac459e94864b03d69923bd0ada6eea01bc
Nikhil S <159870415+Nickkilll@users.noreply.github.com>
2024-02-21 15:41:19 +0530

    Create Sum of Even and ODD.cpp

---
+++ Control Structures/Sum of Even and ODD.cpp
@@ -0,0 +1,21 @@
+// Write a C program to find the sum and average of even and odd numbers from the given number list.
+
+// Sample Input:
+
+// 1
+
+// 2
+
+// 3
+
+// 4
+
+// -1
+
+// Sample Output:
+// 6
+// 4
+// 3.00
+
+// 2.00
+
